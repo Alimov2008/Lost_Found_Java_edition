24c4ab9f317fb58bbbe4bfcab838f536add2371b
Ali <alimovmuhammadali01222008@gmail.com>
2025-11-29 16:36:43 +0500

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.lost_found.lost_found {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.lost_found.lost_found to javafx.fxml;
+    exports com.lost_found.lost_found;
+}
