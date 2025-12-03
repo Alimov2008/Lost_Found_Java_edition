8edeffafd6bd4a4ce507d7351a1c4a233c19425f
Ali <alimovmuhammadali01222008@gmail.com>
2025-12-03 22:46:12 +0500

    created mainapp javafx

--- src/main/java/app/MainApp.java
+++ src/main/java/app/MainApp.java
@@ -1,7 +1,29 @@
 package app;
 
-public class MainApp {
+import dao.Database;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Database.initialize();
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/app/main.fxml"));
+        Scene scene = new Scene(loader.load());
+        scene.getStylesheets().add(getClass().getResource("/app/styles.css").toExternalForm());
+
+        stage.setTitle("Lost & Found - JavaFX");
+        stage.setScene(scene);
+        stage.setWidth(1600);
+        stage.setHeight(900);
+        stage.show();
+    }
+
     public static void main(String[] args) {
-        ConsoleApp.main(args);
+        launch();
     }
 }
