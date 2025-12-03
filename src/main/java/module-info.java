2b32fa4f2c31c97f8754f91b3d08665a1739fa94
Ali <alimovmuhammadali01222008@gmail.com>
2025-12-03 10:22:52 +0500

    java module update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,16 @@
 module app {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.web;
     requires java.sql;
 
     opens app to javafx.fxml;
     opens app.controllers to javafx.fxml;
+    opens app.dao to javafx.fxml;
     opens app.model to javafx.fxml;
 
     exports app;
     exports app.controllers;
+    exports app.dao;
+    exports app.model;
 }
